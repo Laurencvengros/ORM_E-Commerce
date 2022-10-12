2a33a55a509389444062debf21cbd4271ea9aa66
Lauren <l.cvengros@icloud.com>
2022-10-12 11:26:05 -0400

    added conditions for product tag model

--- models/Product.js
+++ models/Product.js
@@ -15,28 +15,28 @@ Product.init(
     primaryKey: true,
     autoIncrement: true
    },
-   product_name:{
+   product_name: {
     type: DataTypes.STRING,
     allowNull: false,
    },
-   price:{
+   price: {
     type: DataTypes.DECIMAL,
     allowNull: false,
-    validate:{
+    validate: {
       isDecimal: true
     }
    },
-   stock:{
+   stock: {
     type: DataTypes.INTEGER,
     allowNull: false,
     defaultValue: 10,
-    validate:{
+    validate: {
       isNumeric: true
     }
    },
-   category_id:{
+   category_id: {
     type: DataTypes.INTEGER,
-    references:{
+    references: {
       model: 'category',
       key: 'id'
     }
