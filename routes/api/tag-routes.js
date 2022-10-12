602e056795a555980e05b81194747f2b53fe41c9
Lauren <l.cvengros@icloud.com>
2022-10-12 14:52:16 -0400

    fixed wrong variable names on POST request

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,15 +36,16 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try{
-    const updateTag = await Tag.create(req.body); 
-    res.status(200).json(updateTag);
+    const newTag = await Tag.create(req.body); 
+    res.status(200).json(newTag);
   }catch(err){
   res.status(500).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try
 });
 
 router.delete('/:id', (req, res) => {
