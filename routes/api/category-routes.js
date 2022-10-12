ac2929a910d17ac1d40aa4bd58c73f1d3ddcd161
Lauren <l.cvengros@icloud.com>
2022-10-12 12:17:42 -0400

    added put request to update category and post request to add a category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,16 +31,37 @@ router.get('/:id', async (req, res) => {
  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  }catch(err){
+  res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      },
+    });
+    if(updateCategory){
+      res.json(updateCategory);
+    }else{
+      res.status(404).json({message: 'No category with that id!'});
+    }
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  
 });
 
 module.exports = router;
